b91a84d145fdbe348d26aca2dcf86993669303e0
Sophie Muspratt <sophie.muspratt@travisperkins.co.uk>
2016-09-14 11:39:40 +0100

    Fixing failing test for factors of 4

--- PrimeNumbers/src/primeNumbers/PrimeNumbers.java
+++ PrimeNumbers/src/primeNumbers/PrimeNumbers.java
@@ -9,10 +9,11 @@ public class PrimeNumbers
 
 	public List<Integer> primeFactorsOf(int prime)
 	{
-		List<Integer> primeList = new ArrayList<>();
+		ArrayList<Integer> primeList = new ArrayList<>();
 		
-		if (prime % 2 == 0){
-			primeList.add(prime);
+		while (prime % 2 == 0){
+			primeList.add(2);
+			prime /= 2;
 		}
 		
 		if (prime % 3 == 0){
