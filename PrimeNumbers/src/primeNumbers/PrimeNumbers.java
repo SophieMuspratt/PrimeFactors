0a7d7a9a3e5cbd9f08eb9c5289b504b10755c03f
Sophie Muspratt <sophie.muspratt@travisperkins.co.uk>
2016-09-14 10:56:00 +0100

    Fixing failing test for factors of 3

--- PrimeNumbers/src/primeNumbers/PrimeNumbers.java
+++ PrimeNumbers/src/primeNumbers/PrimeNumbers.java
@@ -10,10 +10,15 @@ public class PrimeNumbers
 	public List<Integer> primeFactorsOf(int prime)
 	{
 		List<Integer> primeList = new ArrayList<>();
+		
 		if (prime % 2 == 0){
 			primeList.add(prime);
 		}
-
+		
+		if (prime % 3 == 0){
+			primeList.add(prime);
+		}	
+		
 		return primeList;
 
 	}
